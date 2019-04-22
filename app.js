eecc9d0ff072bb6084a094f4af4e6d9372a4c687
Justin Ziegler <justinziegler345@gmail.com>
2019-04-22 01:00:01 -0400

    moved to yarn

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 12345;
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from ScoreCast.");
+});
+
+app.listen(port);
+console.log(`ScoreCast backend running on ${port}`);
